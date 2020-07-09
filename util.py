486f3cf10a84e7d5742764c012cf90f8f3b1efcc
Gaurav Sekhri <59785364+gauravsekhri@users.noreply.github.com>
2020-07-09 22:57:01 +0530

    Update util.py

--- util.py
+++ util.py
@@ -33,12 +33,12 @@ def load_saved_artifacts():
     global __data_columns
     global __locations
 
-    with open("./artifacts/columns.json", 'r') as f:
+    with open("columns.json", 'r') as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
     global __model
-    with open("./artifacts/bengaluru.pickle", 'rb') as f:
+    with open("bengaluru.pickle", 'rb') as f:
         __model = pickle.load(f)
     print("loading saved artifacts....")
 
